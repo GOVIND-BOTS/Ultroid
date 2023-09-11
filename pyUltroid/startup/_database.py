db5fc3f3320f6973c5c1abef562ee0db109a3244
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-09-11 19:33:15 +0530

    _database.py

--- pyUltroid/startup/_database.py
+++ pyUltroid/startup/_database.py
@@ -267,8 +267,8 @@ class RedisDB(_BaseDatabase):
             import sys
 
             sys.exit()
-        kwargs["redis-13549.c302.asia-northeast1-1.gce.cloud.redislabs.com"] = host
-        kwargs["RCr5uV8kB11l1swHqGLgJcd7Eqxk83Qy"] = password
+        kwargs[""] = host
+        kwargs[""] = password
         kwargs["port"] = port
 
         if platform.lower() == "qovery" and not host:
